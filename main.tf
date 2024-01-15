9e544e967ff9d57bf80982f3b052484e6c9b950d
ranjeet <ranjeet@email.com>
2024-01-15 20:47:48 +0530

    sg

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0d3f444bc76de0a79"
   instance_type = "t2.micro"
   key_name = "my-key"
+  security_groups = "my-sg"
 
   tags = {
     Name = "MyInstance"
@@ -17,4 +18,43 @@ resource "aws_key_pair" "key" {
 resource "aws_eip" "lb" {
   instance = aws_instance.web.id
   domain   = "vpc"
+}
+
+resource "aws_default_vpc" "default" {
+  tags = {
+    Name = "Default VPC"
+  }
+}
+resource "aws_security_group" "sg" {
+  name        = "my-sg"
+  description = "Allow ssh & http"
+  vpc_id      = aws_default_vpc.default.id
+
+  tags = {
+    Name = "my-sg"
+  }
+
+  egress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = tcp
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = tcp
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
