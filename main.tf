a7307784f90665a103178801f434fb0bb6ded36a
ranjeet <ranjeet@email.com>
2024-01-15 20:09:18 +0530

    add

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ data "aws_ami" "amzn-linux-2023-ami" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.amzn-linux-2023-ami.id
-  instance_type = "t2.micro"
+  instance_type = "c6a.2xlarge"
   subnet_id     = aws_subnet.sub-1.id
 
   cpu_options {
